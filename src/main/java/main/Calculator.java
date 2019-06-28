2f4446f77a34980841175822a99bfd901c956f78
V-RAJ <V-RAJ@DESKTOP-PK0NDMB>
2019-06-28 17:32:25 +0530

    Done

--- src/main/java/main/Calculator.java
+++ src/main/java/main/Calculator.java
@@ -2,6 +2,7 @@ package main;
 import java.util.*; 
 
 public class Calculator {
+	
     public static void main(String[] args)
     {
         System.out.println("Enter first and second number:");
@@ -39,6 +40,7 @@ public class Calculator {
     {
     	int result=0;
 //        write your code here 
+    	result=x+y;
     	
         return result;
     }
@@ -46,18 +48,21 @@ public class Calculator {
     {
     	int result=0;
 //        write your code here
+    	result=x-y;
         return result;
     }
     public static int mult(int x, int y)
     {
     	int result=0;
-//      write your code here        
+//      write your code here
+    	result=x*y;
     	return result;
     }
     public static int div(int x, int y)
     {
     	int result=0;
 //      write your code here
+    	result=x/y;
     	return result;
     }
 
